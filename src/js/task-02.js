cb38151bfa7303c687f45f88b054fcd9dc3ea41c
Yuliia Karas <iuliiakaras@gmail.com>
2023-03-19 09:09:06 +0200

    update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+console.log(document);
+const liElement = document.createElement("li");
+liElement.textContent = "Potatoes";
+console.log(liElement);
+
+const body = document.body;
+console.log(body);
+
+const listIngredients = document.querySelector("#ingredients");
+console.log(listIngredients);
+
+const addedlistIngredients = ingredients.map((ingredient) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("liElement");
+  return liElement;
+});
+
+listIngredients.append(...addedlistIngredients);
