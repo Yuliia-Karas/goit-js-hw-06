e378e34d1740a7cf2c601fb5f3196454e2dad806
Yuliia Karas <iuliiakaras@gmail.com>
2023-03-21 11:06:01 +0200

    task-09 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+console.dir(body);
+
+const btn = document.querySelector(".change-color");
+console.dir(btn);
+
+const spanColor = document.querySelector(".color");
+console.dir(spanColor);
+
+btn.addEventListener("click", () => {
+  const generatedColor = getRandomHexColor();
+  document.body.style.backgroundColor = generatedColor;
+  spanColor.textContent = generatedColor;
+});
+
